e31e73040ce9f5601118a5c923491c6899bba995
Fernanda Oliveira <70150556+Fernanda-a@users.noreply.github.com>
2025-10-06 21:14:26 -0300

    Update StringAndDates.java
    Added date structure and a mini challenge

--- firstSteps/StringAndDates.java
+++ firstSteps/StringAndDates.java
@@ -30,8 +30,38 @@ public class StringAndDates {
 
   //Dates
 
-  
-  
+  // Cadastro do cliente com data de nascimento
+  LocalDate birthDate = LocalDate.of(1990, 5, 15);
+  System.out.println("Client's birthdate: " + birthDate);
+
+  // Data e hora de cadastro do cliente
+  LocalDateTime dateHourSignUp = LocalDateTime.now();
+  System.out.println("Sign up's date and time: " + dateHourSignUp);
+
+  // Data e hora de cadastro com fuso horário (ZonedDateTime)
+  ZonedDateTime dateHourSignUpWithTimeZone = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"));
+  System.out.println("Date and hour with time zone (São Paulo): " + dateHourSignUpWithTimeZone);
+
+  // Calculando o próximo aniversário do cliente
+  LocalDate today = LocalDate.now();
+  LocalDate nextBirthday = birthDate.withYear(today.getYear());
+
+  if (nextBirthday.isBefore(today) || nextBirthday.isEqual(today)) {
+      nextBirthday = nextBirthday.plusYears(1);
+  }
+  System.out.println("Client's next birthday: " + nextBirthday);
+
+  // Calculando o período até o próximo aniversário
+  Period PeriodOfTime = Period.between(today, nextBirthday);
+  System.out.println(PeriodOfTime.getMonths() + " months and " + PeriodOfTime.getDays() + " until your next birthday.");
+
+  // Verificando se o aniversário é antes ou depois de um evento especí­fico
+  LocalDate dataEvento = LocalDate.of(2024, 10, 11);
+  if (nextBirthday.isAfter(dataEvento)) {
+      System.out.println("The birthday is after the event");
+  } else {
+      System.out.println("The birthday was before of is the same day as the event.");
+  } 
     
   }
 }
